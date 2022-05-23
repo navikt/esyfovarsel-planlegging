c19317b45acc57cee9c8310e79727e3a5f9da6f6
Tor-Christian Tangenes <torchrct@gmail.com>
2022-05-23 16:05:48 +0200

    F

--- job/src/main/kotlin/no/nav/syfo/Job.kt
+++ job/src/main/kotlin/no/nav/syfo/Job.kt
@@ -11,12 +11,8 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.runBlocking
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 const val jobTriggerUrl = "https://esyfovarsel-planlegging.dev.intern.nav.no/api/job/trigger"
-
-val log: Logger = LoggerFactory.getLogger("no.nav.syfo.Job")
 
 fun httpClient(): HttpClient {
     return HttpClient(CIO) {
@@ -32,16 +28,12 @@ fun httpClient(): HttpClient {
 }
 
 fun main() {
-    log.info("JOB: Kjører jobb")
     runBlocking {
         val response: HttpResponse = httpClient().post(jobTriggerUrl)
 
         val status = response.status
         if (status == HttpStatusCode.OK) {
             val varslerSendt = response.receive<Int>()
-            log.info("Varsler sendt: $varslerSendt")
-        } else {
-            log.error("Feil i esyfovarsel-planlegging-job: Klarte ikke kalle trigger-API i esyfovarsel-planlegging. Fikk svar med status: $status")
         }
     }
 }
